b6b841551b12cbe5c78ff6d3e69c42c42743f0e1
thietdmt <69181423+thietdmt@users.noreply.github.com>
2021-01-09 16:46:52 +0700

    Update mitpage.js

--- mitpage.js
+++ mitpage.js
@@ -1,1 +1,39 @@
-
+function getData(){
+	var dataAMZ = {
+	B07RGDFZ5Q: {US: "https://amzn.to/37DyOjs",CA: "",UK: ""},
+	B00VAG84HY: {US: "https://amzn.to/2K72BZy",CA: "",UK: ""},
+	B00EZZ5KBU: {US: "https://amzn.to/3lXgsyQ",CA: "",UK: ""},
+	B000A7NN4I: {US: "https://amzn.to/2JDeA1v",CA: "",UK: ""},
+	B00007J5U7: {US: "https://amzn.to/3oA8lKk",CA: "",UK: ""},
+	B086PQ8R7B: {US: "https://amzn.to/3qtlJBO",CA: "",UK: ""},
+	B07VFVZ95T: {US: "https://amzn.to/2LLwwYc",CA: "",UK: ""},
+	B001GP81R2: {US: "https://amzn.to/398hRhL",CA: "",UK: ""},
+	B01LYZJ4WM: {US: "https://amzn.to/2Xl8o1i",CA: "",UK: ""},
+	B00AEMGGU2: {US: "https://amzn.to/3s5Fv78",CA: "",UK: ""},
+	B00T9RANUC: {US: "https://amzn.to/2LowCF3",CA: "",UK: ""},
+	B07KPN33YD: {US: "https://amzn.to/3hOCUtQ",CA: "",UK: ""},
+	B079L5D89G: {US: "https://amzn.to/2Le9Qjk",CA: "",UK: ""},
+	B01NAUZJPE: {US: "https://amzn.to/3hNJSze",CA: "",UK: ""},
+	B01M0N8256: {US: "https://amzn.to/3hPomtZ",CA: "",UK: ""},
+	B07PWQF712: {US: "https://amzn.to/3pSL1Io",CA: "",UK: ""},
+	B012J1TFTE: {US: "https://amzn.to/3nn5yn4",CA: "",UK: ""},
+	B00D4NPMJE: {US: "https://amzn.to/35iTCw6",CA: "",UK: ""},
+	B005A4ON90: {US: "https://amzn.to/3s5Ghky",CA: "",UK: ""},
+	B009X43F38: {US: "https://amzn.to/2XjuRvA",CA: "",UK: ""},
+	B00272N3DK: {US: "https://amzn.to/3hUzMg6",CA: "",UK: ""},
+	B07DTKXTXV: {US: "https://amzn.to/2XkMRFW",CA: "",UK: ""},
+	B01DV3849Q: {US: "https://amzn.to/2LtuQm2",CA: "",UK: ""},
+	B076BSQVZT: {US: "https://amzn.to/38kChVM",CA: "",UK: ""},
+	B00R6AW5S2: {US: "https://amzn.to/3pZfqFi",CA: "",UK: ""},
+	B077N51CR2: {US: "https://amzn.to/35kTSee",CA: "",UK: ""},
+	B01H4TKED2: {US: "https://amzn.to/3olrpwi",CA: "",UK: ""},
+	B07HP13GFK: {US: "https://amzn.to/3hS6D53",CA: "",UK: ""},
+	B07FX7Z3NL: {US: "https://amzn.to/38mnhqq",CA: "",UK: ""},
+	B0018RSEMU: {US: "https://amzn.to/2JYOOou",CA: "",UK: ""},
+	B002FU5SKU: {US: "https://amzn.to/3biRMiF",CA: "",UK: ""},
+	B014VBOIOC: {US: "https://amzn.to/38mnAl4",CA: "",UK: ""},
+	B07D2V7TMY: {US: "https://amzn.to/39fvmwj",CA: "",UK: ""},
+	B07CN38M23: {US: "https://amzn.to/3orDsbo",CA: "",UK: ""}
+	}
+	return dataAMZ;
+}
